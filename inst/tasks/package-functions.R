49d5723b82b5e9684a9e93daabae3c4f4df1fd21
Filip Krikava <krikava@gmail.com>
2020-04-27 14:20:48 +0000

    Add elipisis info into package-functions

--- inst/tasks/package-functions.R
+++ inst/tasks/package-functions.R
@@ -16,7 +16,21 @@ package <- basename(args[1])
 
 ns <- getNamespace(package)
 exports <- getNamespaceExports(package)
-functions <- ls(env=ns, all.names=TRUE)
+bindings <- ls(env=ns, all.names=TRUE)
 
-df <- tibble(functions, exported=functions %in% exports)
+function_bindings <- sapply(bindings, function(x) {
+    f <- get0(x, envir=ns)
+    if (!is.function(f)) NA else x
+})
+function_bindings <- na.omit(function_bindings)
+functions <- lapply(function_bindings, get0, envir=ns)
+
+params <- lapply(functions, function(x) names(formals(x)))
+
+num_params <- sapply(params, length)
+
+has_elipsis <- sapply(params, function(x) "..." %in% x)
+
+df <- tibble(fun=function_bindings, num_params, has_elipsis, exported=function_bindings %in% exports)
+
 write_csv(df, OUTPUT_FILE)
