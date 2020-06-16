87d4b5833a2049f1d241f13353f710b4acb0014d
Filip Krikava <krikava@gmail.com>
2020-06-16 09:37:31 +0000

    Update R install script
    Fix version number
    Fix make options
    Sync flags and confiure options

--- inst/install-r.sh
+++ inst/install-r.sh
@@ -5,7 +5,7 @@ set -o xtrace
 
 CFLAGS=${CFLAGS:-""}
 CONFIGURE_OPTS=${CONFIGURE_OPTS:-""}
-MAKE_OPTS=${MAKE_OPTS:-"-j70"}
+MAKE_OPTS=${MAKE_OPTS:-"-j"}
 
 : "${CRAN_MIRROR_URL:?Not set}"
 : "${R_PROJECT_BASE_DIR:?Not set}"
@@ -18,10 +18,22 @@ DEST_DIR="$R_PROJECT_BASE_DIR/R"
 [ -d "$DEST_DIR" ] || mkdir -p "$DEST_DIR"
 
 cd "$R_PROJECT_BASE_DIR/R"
-curl "$CRAN_MIRROR_URL/src/base/R-3/R-$R_VERSION_FULL.tar.gz" | tar -xzf -
+curl "$CRAN_MIRROR_URL/src/base/R-${R_VERSION_FULL:0:1}/R-$R_VERSION_FULL.tar.gz" | tar -xzf -
 
 cd "R-$R_VERSION_FULL"
-./configure --prefix="$DEST_DIR/R-$R_VERSION_FULL" "$CONFIGURE_OPTS"
+
+export CPPFLAGS="-g3 -O2 -ggdb3"
+export CFLAGS="-g3 -O2 -ggdb3"
+export R_KEEP_PKG_SOURCE=yes
+export CXX="g++"
+
+./configure --prefix="$DEST_DIR/R-$R_VERSION_FULL" \
+    --with-blas --with-lapack --without-ICU --with-x \
+    --with-tcltk --without-aqua --with-recommended-packages \
+    --without-internal-tzcode --with-included-gettext \
+    --disable-byte-compiled-packages \
+    "$CONFIGURE_OPTS"
+
 make "$MAKE_OPTS"
 make "$MAKE_OPTS" install
 
