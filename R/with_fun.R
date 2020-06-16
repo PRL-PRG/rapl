a87a0e859798e5a2e121fec1df571f1744440279
Filip Krikava <krikava@gmail.com>
2020-06-16 09:00:05 +0000

    Rewrite with_fun using lightr

--- R/with_fun.R
+++ R/with_fun.R
@@ -1,27 +1,48 @@
+library(lightr)
+
 #' @importFrom injectr inject_code
 with_capturing_calls <- function(funs, code) {
-  if (is.function(funs)) {
-    funs <- list(funs)
-  }
 
-  old_bodies <- lapply(funs, body)
+    call_exit_callback <- function(context, application, package, func, call) {
+        call_expression <- get_expression(call)
+        fun_name <- get_name(func)
+        parameters <- get_parameters(call)
+        expressions <- list()
+        values <- list()
+        for(parameter in parameters) {
+            arguments <- get_arguments(parameter)
+            argument <- arguments[[1]]
+            expressions[[get_name(parameter)]] <- get_expression(argument)
+        }
 
-  inject_code <- substitute({
-    if (isTRUE(getOption(".tracing"))) {
-      options(.tracing=FALSE)
-      (function(call) {
-        call <- as.list(call)
-        fun <- as.character(call[[1]])
-        args <- call[-1]
-        retval <- returnValue()
+        call_stack <- get_call_stack(application)
+
+        print(call_stack)
+
+        return_value <- if(is_successful(call)) get_result(call) else NULL
+
+        ## resolve parse
+        if (fun_name %in% c("eval", "evalq")) {
+            expr_arg <- get_arguments(get_parameters(call)[[1]])[[1]]
+            eval_expr <- get_expression(expr_arg)
+
+            parse_value <- if (is.call(eval_expr) &&
+                               identical(eval_expr[[1]], as.name("parse"))) {
+                tryCatch({
+                    eval.parent(eval_expr, 3)
+                }, error=function(e) {
+                    NULL
+                })
+            }
+        }
 
         st <- sys.calls()
-        st_start <- which(sapply(st, function(x) x[[1]] == "with_capturing_calls"))
+        st_start <- which(sapply(st, function(x) identical(x[[1]], as.name("with_capturing_calls"))))
         # +1 for with_capturing_calls
         # +1 for tryCatch
         # +1 for tryCatchList
-        # +1 for force - FIXME do we need it?
-        st_start <- st_start+4
+                                        # +1 for force - FIXME do we need it?
+        st_start <- st_start+3
         st_end <- length(st)
         # -1 for the actual eval
         # -1 for this function
@@ -34,60 +55,30 @@ with_capturing_calls <- function(funs, code) {
         st <- st[st_start:st_end]
         st <- rev(st)
 
-        # TODO: this should be passed in as an argument
-        resolve_parse <- function(call) {
-          if (call$fun %in% c("eval", "evalq")) {
-            expr <- call$args$expr
-            if (is.call(expr)
-                && identical(expr[[1]], as.name("parse"))) {
-              tryCatch({
-                call$args$expr <- eval.parent(expr, 3)
-                call$parsed_call <- expr
-              }, error=function(e) {
-                call$parsed_call <- NULL
-              })
-            }
-          }
-          call
-        }
-       
         value <- list(
-          fun=fun,
-          args=args,
-          retval=retval,
-          st=st
+            fun=fun_name,
+            args=expressions,
+            retval=return_value,
+            st=st,
+            parse_value = parse_value
         )
 
-        value <- resolve_parse(value)
-       
-        capture <- getOption(".capture")
-        n <- length(capture)
-        assign(as.character(n+1), value, envir=capture)
-      })(match.call())
-      options(.tracing=TRUE)
+        env <- get_data(context)
+        n <- length(env)
+        assign(as.character(n+1), value, envir=env)
     }
-  })
 
-  withr::local_options(list(.capture=new.env(parent=emptyenv())))
+    context <- create_context(call_exit_callback=call_exit_callback, functions=funs)
 
-  for (f in funs) {
-    injectr::inject_code(inject_code, f, "onexit")
-  }
+    set_data(context, new.env(parent=emptyenv()))
 
-  tryCatch({
-    options(.tracing=TRUE)
-    force(code)
-  }, finally={
-    options(.tracing=FALSE)
-    for (idx in seq_along(funs)) {
-      injectr:::reassign_function_body(funs[[idx]], old_bodies[[idx]])
-    }
-  })
+    trace_code(code, context, envir=environment())
 
-  ret <- as.list(getOption(".capture"))
-  names(ret) <- NULL
-  ret
+    captures <- as.list(get_data(context))
+    names(captures) <- NULL
+    captures
 }
+
 
 #' @export
 with_capturing_calls_to_file <- function(funs, file, code) {
@@ -96,3 +87,4 @@ with_capturing_calls_to_file <- function(funs, file, code) {
     saveRDS(calls, file)
   }
 }
+
