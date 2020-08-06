23527b86d2e56ee2d677fdff655e855f212b1cd0
Filip Krikava <krikava@gmail.com>
2020-08-06 15:47:17 +0000

    Do not do make install when installing R

--- inst/install-r.sh
+++ inst/install-r.sh
@@ -60,15 +60,19 @@ export CFLAGS="-g3 -O2 -ggdb3"
 export R_KEEP_PKG_SOURCE=yes
 export CXX="g++"
 
-./configure --prefix="$(pwd)" \
-    --with-blas --with-lapack --without-ICU --with-x \
-    --with-tcltk --without-aqua --with-recommended-packages \
-    --without-internal-tzcode --with-included-gettext \
+./configure \
     --disable-byte-compiled-packages \
+    --without-aqua \
+    --without-ICU \
+    --without-internal-tzcode \
+    --with-blas \
+    --with-included-gettext \
+    --with-lapack \
+    --with-recommended-packages \
+    --with-tcltk \
+    --with-x \
     "$CONFIGURE_OPTS"
 
 make "$MAKE_OPTS"
-make "$MAKE_OPTS" install
-make "$MAKE_OPTS" install-tests
 
 popd
