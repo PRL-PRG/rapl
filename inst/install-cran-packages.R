d5fd6fb00ba45fd0d98e18bff6f4335f2f97ab18
Filip Krikava <krikava@gmail.com>
2020-08-11 14:13:29 +0000

    Fix typos in install-cran-packages.R

--- inst/install-cran-packages.R
+++ inst/install-cran-packages.R
@@ -15,11 +15,11 @@ install_cran_packages <- function(mirror,
   installed <- installed.packages(lib.loc=lib)[,1]
   missing <- setdiff(requested, installed)
 
-  message("Installing ", length(missing), " packages from $mirror into $libs")
+  message("Installing ", length(missing), " packages from ", mirror, " into ", lib)
 
   if (length(missing) > 0) {
-    if (!dir.exists(destdir)) dir.create(destdir, recursive=TRUE)
-    if (!dir.exists(lib)) dir.create(lib, recursive=TRUE)
+    if (!is.null(destdir) && !dir.exists(destdir)) dir.create(destdir, recursive=TRUE)
+    if (!is.null(lib) && !dir.exists(lib)) dir.create(lib, recursive=TRUE)
   }
 
   res <- install.packages(
@@ -40,18 +40,3 @@ install_cran_packages <- function(mirror,
     append=file.exists(output_file)
   )
 }
-
-show_help <- function() {
-  cat(
-    "Usage: install-cran-packages.R [-d PATH] [-f FILE] [-l PATH] [-m HOST]",
-    "",
-    "where:",
-    "",
-    "  -d PATH      where to keep downloaded sources (optional)",
-    "  -f FILE      list of packages (defaults to all avalable packages)",
-    "  -l PATH      where to install the packages (defaults to $def_lib)",
-    "  -m HOST      mirror to use (defaults to $def_mirror)",
-    sep="\n"
-  )
-}
-
