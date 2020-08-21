e0110f01b33ef5fb5c67a8c9226c33e433c3988d
Filip Krikava <krikava@gmail.com>
2020-08-21 12:04:08 +0000

    Silent start-xvfb

--- inst/start-xvfb.sh
+++ inst/start-xvfb.sh
@@ -5,7 +5,6 @@ if [ -z "$NO_DISPLAY" -a -z "$DISPLAY" ]; then
 
     pid=$(pgrep Xvfb)
     if [ $? -ne 0 ]; then
-        echo "Starting Xvfb..."
         nohup Xvfb $local_display -screen 0 1280x1024x24 >/dev/null 2>&1 &
         pid=$!
         # give Xvfb a bit of a time before it is initialized
