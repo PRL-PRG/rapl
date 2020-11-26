6923dde52a39248e6fffeb90f4058f54f8c567f7
Filip Krikava <krikava@gmail.com>
2020-11-26 22:36:22 +0000

    Update echo task

--- inst/tasks/echo.R
+++ inst/tasks/echo.R
@@ -3,5 +3,8 @@
 options(error = function() { traceback(3); q(status=1) })
 
 args <- commandArgs(trailingOnly=TRUE)
-print(Sys.getenv())
-print(args)
+cat("*** ENV:\n")
+cat(Sys.getenv(), sep="\n")
+cat("\n\n")
+cat("*** ARGS:\n")
+cat(args, sep="\n")
