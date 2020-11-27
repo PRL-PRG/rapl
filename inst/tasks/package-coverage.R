eeb82760195259885def688be659a5ed9b672e73
Filip Krikava <krikava@gmail.com>
2020-11-27 19:51:13 +0000

    Add param handling to package-coverage task

--- inst/tasks/package-coverage.R
+++ inst/tasks/package-coverage.R
@@ -2,21 +2,21 @@
 
 options(error = function() { traceback(3); q(status=1) })
 
-library(covr)
-library(fs)
-library(purrr)
 library(readr)
 library(stringr)
 library(tibble)
+library(optparse)
 
 COVERAGE_FILE <- "coverage.csv"
 COVERAGE_DETAILS_FILE <- "coverage-details-{by}.csv"
+COVERAGE_RAW_FILE <- "coverage-raw-{type}.RDS"
 COVERAGE_BY <- c("line", "expression")
-ALL_TYPES <- eval(formals(package_coverage)$type)
 
-do_coverage <- function(type) {
-  pc <- package_coverage(path, type=type, quiet=FALSE)
-  saveRDS(pc, str_glue("coverage-raw-{type}.RDS"))
+do_coverage <- function(path, type, quiet, clean) {
+  library(covr)
+
+  pc <- package_coverage(path, type=type, quiet=quiet, clean=clean)
+  saveRDS(pc, str_glue(COVERAGE_RAW_FILE))
 
   df <- tibble(type, error=NA)
 
@@ -26,7 +26,7 @@ do_coverage <- function(type) {
     cvr <- tally_coverage(pc, by=by)
     cvr <- add_column(cvr, type=type, .before="filename")
 
-    write_csv(cvr, file, append=file_exists(file))
+    write_csv(cvr, file, append=file.exists(file))
 
     pct <- percent_coverage(cvr, by=by)
 
@@ -36,48 +36,84 @@ do_coverage <- function(type) {
   df
 }
 
-do_coverage_checked <- function(type) {
-  tryCatch(do_coverage(type), error=function(e) tibble(type, error=e$message))
-}
-
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args) < 2) {
-  stop("Usage: <path-to-package-src-dir> <--all|--tests|--examples|--vignettes>")
-}
-
-path <- args[1]
-types <- c()
-
-for (arg in args[2:length(args)]) {
-  if (startsWith(arg, "--")) {
-    val <- substr(arg, 3, nchar(arg))
-    if (val %in% ALL_TYPES) types <- append(types, val)
-    else stop("unknown arg: ", arg)
-  } else {
-    stop("unknown arg: ", arg)
+run <- function(path, options) {
+  str(options)
+  for (by in COVERAGE_BY) {
+    file <- str_glue(COVERAGE_DETAILS_FILE)
+    if (file.exists(file)) {
+      file.remove(file)
+    }
   }
-}
 
-if (length(types) == 0) {
-  stop("Missing types arguments (e.g. --all, --tests, ...)")
-}
+  Sys.setenv(
+    R_TESTS="",
+    R_BROWSER="false",
+    R_PDFVIEWER="false",
+    R_BATCH="1"
+  )
 
-for (by in COVERAGE_BY) {
-  file <- str_glue(COVERAGE_DETAILS_FILE)
-  if (file_exists(file)) {
-    file_delete(file)
+  path <- normalizePath(path, mustWork=TRUE)
+  pkg_src <- tempfile()
+  stopifnot(dir.create(pkg_src, recursive=TRUE))
+  message("Using: ", pkg_src, " as temp file for package source")
+  stopifnot(file.copy(str_c(path, .Platform$file.sep, .Platform$file.sep), pkg_src, recursive=TRUE))
+  if (options$clean) {
+    on.exit(unlink(pkg_src, recursive=TRUE))
   }
+
+  types <- str_split(options$type, ",")[[1L]]
+  print(types)
+
+  coverage <- lapply(types, function(type) {
+    tryCatch({
+      do_coverage(
+        pkg_src,
+        type=type,
+        quiet=options$quiet,
+        clean=options$clean
+      )
+    }, error=function(e) {
+      message("Error getting coverage for ", pkg_src, " type ", type, ": ", e$message)
+      tibble(type, error=e$message)
+    })
+  })
+
+  coverage_df <- do.call(rbind, coverage)
+
+  write_csv(coverage_df, COVERAGE_FILE)
+
+  stopifnot(all(is.na(coverage_df$error)))
 }
 
-Sys.setenv(
-  R_TESTS="",
-  R_BROWSER="false",
-  R_PDFVIEWER="false",
-  R_BATCH="1"
+option_list <- list(
+  make_option(
+    c("-q", "--quiet"), action="store_true", default=FALSE,
+    dest="quiet", help="Print extra output [default]"
+  ),
+  make_option(
+    c("--no-clean"), action="store_false", default=TRUE,
+    dest="clean", help="Clean the intermediate output"
+  ),
+  make_option(
+    c("--type"), default="all,examples,tests,vignettes",
+    help="Coma separated list of things to run individually [default %default]",
+    metavar="TYPE"
+  )
 )
 
-coverage <- map_dfr(types, do_coverage_checked)
+opt_parser <- OptionParser(option_list=option_list)
+opts <- parse_args(opt_parser, positional_arguments=1)
 
-write_csv(coverage, COVERAGE_FILE)
+if (length(opts$args) != 1) {
+  message("Missing path to a package source")
+  q(1, save="no")
+}
 
-stopifnot(all(is.na(coverage$error)))
+package_path <- opts$args
+
+if (!dir.exists(package_path)) {
+  message(package_path, ": no such directory")
+  q(1, save="no")
+}
+
+run(package_path, opts$options)
