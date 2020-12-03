b2da28abbc38bb7fadd05f7ed5fa04ac40f1df53
Filip Krikava <krikava@gmail.com>
2020-12-03 10:50:45 +0000

    Update the map.sh wrapper to be more flexible

--- inst/map.sh
+++ inst/map.sh
@@ -6,6 +6,9 @@ CMD="$0 $@"
 TIMEOUT=${RUNR_TIMEOUT:-30m}
 JOBS=${RUNR_JOBS:-1}
 OUTPUT_DIR=${RUNR_OUTPUT_DIR:-run}
+INPUT_FILE=${RUNR_INPUT_FILE:-'-'}
+EXEC_WRAPPER=${RUNR_EXEC_WRAPPER:-"$(dirname $(realpath "$0"))/run-job.sh"}
+DEF_WORK_DIR=${RUNR_WORK_DIR:-'{=1 $_=join("/",@arg) =}'}
 
 usage() {
 cat << EOF
@@ -14,20 +17,24 @@ Usage: $0 [options] [-- <extra args>]
 Options:
   -h | -help            show this help
   -v | --verbose        verbose output
-  -f | --file FILE      the input file (or '-' to indicate stdin)
+  -f | --file FILE      the input file
                         - in the case of a CSV file, each row represents a job
                           and each column an command line argument
                         - in the case of non-csv file, each row represents
                           one argument
+                        (default $INPUT_FILE)
   -o | --output DIR     DIR to store jobs output
                         (default $OUTPUT_DIR)
-  -t | -timeout TIME    timeout in seconds or with a suffix m,h,d
+  -t | --timeout TIME   timeout in seconds or with a suffix m,h,d
                         (default $TIMEOUT)
-  -j | -jobs NUM        number of jobs
+  -j | --jobs NUM       number of jobs
                         (default $JOBS)
-  -e | -exec FILE       a file to execute
+  -e | --exec FILE      a file to execute
                         it will be executed for each job with arguments from
                         the input file followed by all <extra args>
+  -w | --workdir DIR    a subdirectory of output which will be used as working directory for a job
+                        (default output/$DEF_WORK_DIR) all input parameters separated by '/'
+
 
   any other optionss will be passed to GNU parallel arguments
 
@@ -41,16 +48,10 @@ EOF
 }
 
 RUNR_INST_DIR=$(dirname $(realpath "$0"))
-EXEC_WRAPPER=${RUNR_EXEC_WRAPPER:-"$RUNR_INST_DIR/run-job.sh"}
 EXEC_EXTRA_ARGS=""
 PARALLEL_EXTRA_ARGS="--halt now,fail=25%"
 PARSING_EXEC_EXTRA_ARGS=0
 VERBOSE=0
-
-[[ -x "$EXEC_WRAPPER" ]] || {
-    echo "$EXEC_WRAPPER: exec wrapper is missing" >&2
-    exit 1
-}
 
 parse_arg() {
     if [[ -n "$2" ]] && [[ ${2:0:1} != "-" ]]; then
@@ -62,6 +63,7 @@ parse_arg() {
 }
 
 while (( "$#" )); do
+  if [[ $PARSING_EXEC_EXTRA_ARGS -eq 0 ]]; then
     case "$1" in
         -h|--help)
             usage
@@ -92,23 +94,37 @@ while (( "$#" )); do
             parse_arg TIMEOUT $2
             shift 2
             ;;
+        -w|--workdir)
+            parse_arg WORK_DIR $2
+            shift 2
+            ;;
         --)
             PARSING_EXEC_EXTRA_ARGS=1
             shift
             ;;
         *)
-            if [[ $PARSING_EXEC_EXTRA_ARGS -eq 1 ]]; then
-                EXEC_EXTRA_ARGS="$EXEC_EXTRA_ARGS $1"
-            else
-                PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS $1"
-            fi
+            PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS $1"
             shift
             ;;
     esac
+  else
+    EXEC_EXTRA_ARGS="$EXEC_EXTRA_ARGS $1"
+    shift
+  fi
 done
 
 if [[ $VERBOSE -ge 2 ]]; then
     set -o xtrace
+fi
+
+
+if [[ "$EXEC_WRAPPER" != "-" ]]; then
+  if [[ ! -x "$EXEC_WRAPPER" ]]; then
+    echo "$EXEC_WRAPPER: exec wrapper is missing" >&2
+    exit 1
+  fi
+else
+  EXEC_WRAPPER=""
 fi
 
 if [[ "$EXEC" == *"/"* ]]; then
@@ -116,17 +132,18 @@ if [[ "$EXEC" == *"/"* ]]; then
   EXEC=$(realpath "$EXEC")
 fi
 
-if [[ ! -f "$INPUT_FILE" ]]; then
-  echo "$INPUT_FILE: no such file"
-  exit 1
-fi
-INPUT_FILE=$(realpath "$INPUT_FILE")
-
-if [[ "$INPUT_FILE" == *csv ]]; then
-  PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS --csv"
-fi
 if [[ "$INPUT_FILE" != "-" ]]; then
-  PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS -a $INPUT_FILE"
+  if [[ ! -f "$INPUT_FILE" ]]; then
+    echo "$INPUT_FILE: no such file"
+    exit 1
+  else
+    INPUT_FILE=$(realpath "$INPUT_FILE")
+    PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS -a $INPUT_FILE"
+
+    if [[ "$INPUT_FILE" == *csv ]]; then
+      PARALLEL_EXTRA_ARGS="$PARALLEL_EXTRA_ARGS --csv"
+    fi
+  fi
 fi
 
 if [[ -d "$OUTPUT_DIR" ]]; then
@@ -143,15 +160,21 @@ JOBS_FILE="$OUTPUT_DIR/jobs.txt"
 MAP_CMD_FILE="$OUTPUT_DIR/map-cmd.txt"
 TASK_NAME=$(basename "$EXEC")
 
+if [[ -z "$WORK_DIR" ]]; then
+  WORK_DIR=$OUTPUT_DIR/$DEF_WORK_DIR
+fi
+
 echo "$JOBS" > "$JOBS_FILE"
 
 DEBUG_MSG=$(cat <<-EOM
-CMD:     $CMD
-INPUT:   $INPUT_FILE
-OUTPUT:  $OUTPUT_DIR
-TIMEOUT: $TIMEOUT
-JOBS:    $JOBS
-EXEC:    $EXEC
+CMD:      $CMD
+INPUT:    $INPUT_FILE
+OUTPUT:   $OUTPUT_DIR
+TIMEOUT:  $TIMEOUT
+JOBS:     $JOBS
+EXEC:     $EXEC
+WRAPPER:  $EXEC_WRAPPER
+WORK_DIR: $WORK_DIR
 
 additional GNU parallel args: $PARALLEL_EXTRA_ARGS"
 additional exec args: $EXEC_EXTRA_ARGS"
@@ -169,11 +192,11 @@ parallel \
   --bar \
   --env RUNR_RERUN \
   --joblog "$PARALLEL_LOG" \
-  --result "$RESULT_FILE" \
+  --results "$RESULT_FILE" \
   --jobs "$JOBS_FILE" \
   --tagstring "$TASK_NAME - {}" \
   --timeout "$TIMEOUT" \
-  --workdir $OUTPUT_DIR/'{=1 $_=join("/",@arg) =}' \
+  --workdir "$WORK_DIR" \
   $PARALLEL_EXTRA_ARGS \
   "$EXEC_WRAPPER" \
   "$EXEC" \
