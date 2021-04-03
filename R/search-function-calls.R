3624bf7afd2fc9d431870e9e0824387dbe3bf5bd
Filip Krikava <krikava@gmail.com>
2021-04-03 12:13:15 +0000

    Fix search_function_call not to fail on EXPTRSEXP and closures

--- R/search-function-calls.R
+++ R/search-function-calls.R
@@ -1,7 +1,6 @@
 #' @param expr is the expression in which run the search
 #' @param functions is a string vector in the form of package:::function_name
 #' @importFrom stringr str_replace str_c
-#' @importFrom purrr map map2
 #' @export
 #'
 search_function_calls <- function(expr, functions) {
@@ -21,16 +20,18 @@ search_function_calls <- function(expr, functions) {
       } else {
         is_interesting_call(fun)
       }
+    } else if (!is.function(call)) {
+        as.character(call) %in% functions_names
     } else {
-      as.character(call) %in% functions_names
+      FALSE
     }
   }
 
   loop <- function(node) {
-    if (is.atomic(node) || is.name(node)) {
+    if (is.atomic(node) || is.name(node) || typeof(node) == "externalptr") {
       NULL
     } else {
-      calls <- unlist(map(node, loop))
+      calls <- unlist(lapply(node, loop))
 
       if (is.call(node) && is_interesting_call(node)) {
         calls <- append(calls, node)
