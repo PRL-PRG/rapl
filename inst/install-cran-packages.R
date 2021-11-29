ea41c6734343fb3d456f1679ffdce5d138d8c2fb
Filip Krikava <krikava@gmail.com>
2021-11-29 16:08:12 +0100

    Fix package installatio

--- inst/install-cran-packages.R
+++ inst/install-cran-packages.R
@@ -1,30 +1,32 @@
 #!/usr/bin/env Rscript
 
-install_cran_packages <- function(mirror,
-                                  lib=NULL,
-                                  destdir=NULL,
-                                  from=NULL) {
+install_cran_packages <- function(from, libdir, destdir, mirror, force) {
   options(repos=mirror)
 
-  output_file <- file.path(lib, "packages-installed.csv")
-  if (file.exists(output_file)) {
-    unlink(output_file)
+  if (!is.null(destdir)) {
+    if (!dir.exists(destdir)) {
+      dir.create(destdir, recursive=TRUE)
+    }
   }
 
   requested <- if (is.null(from)) {
-    available.packages()[,1]
+    available.packages()[, 1]
   } else {
-    unique(trimws(readLines(from), "both"))
+    packages
   }
 
-  installed <- installed.packages(lib.loc=lib)[,1]
-  missing <- setdiff(requested, installed)
+  if (force) {
+    missing <- packages
+  } else {
+    installed <- installed.packages(lib.loc=libdir)[, 1]
+    missing <- setdiff(requested, installed)
+  }
 
-  message("Installing ", length(missing), " packages from ", mirror, " into ", lib)
+  message("Installing ", length(missing), " packages from ", mirror, " into ", libdir, " sources ", destdir)
 
   if (length(missing) > 0) {
     if (!is.null(destdir) && !dir.exists(destdir)) dir.create(destdir, recursive=TRUE)
-    if (!is.null(lib) && !dir.exists(lib)) dir.create(lib, recursive=TRUE)
+    if (!is.null(libdir) && !dir.exists(libdir)) dir.create(libdir, recursive=TRUE)
   }
 
   # set package installation timeout
@@ -34,18 +36,76 @@ install_cran_packages <- function(mirror,
 
   res <- install.packages(
     missing,
-    lib=lib,
+    lib=libdir,
     destdir=destdir,
     dependencies=TRUE,
     INSTALL_opts=c("--example", "--install-tests", "--with-keep.source", "--no-multiarch"),
     Ncpus=floor(.9*parallel::detectCores())
   )
 
+  invisible(res)
+}
 
-  write.csv(
-    as.data.frame(res, make.names=FALSE),
-    output_file,
-    row.names=FALSE,
-    append=file.exists(output_file)
+args <- commandArgs(trailingOnly=TRUE)
+libdir <- NULL
+destdir <- NULL
+packages <- character()
+mirror <- "http://cran.r-project.org"
+force <- FALSE
+
+shift <- function(n) {
+  if (n > 0) {
+    idx <- -1 * seq(n)
+    args <<- args[idx]
+  }
+}
+
+print_help <- function() {
+  cat(
+    "Usage: install-cran-packages.R [-m URL] [-l DIR] [-d DIR] [<packages>]",
+    "",
+    "options:",
+    "  -m URL \t CRAN mirror to use",
+    "  -l DIR \t where to install (defaults to R_LIBS)",
+    "  -d DIR \t where to download sources (defaults to none)",
+    "  -f     \t force installation",
+    "",
+    "packages:",
+    "  - either a file with one package per line",
+    "  - space separated package names",
+    "  - nothing meaning installing all available packages",
+    sep="\n"
   )
 }
+
+while (length(args) > 0) {
+  if (args[1] == "-l") {
+    stopifnot(length(args) >= 2)
+    libdir <- args[2]
+    shift(2)
+  } else if (args[1] == "-d") {
+    stopifnot(length(args) >= 2)
+    destdir <- normalizePath(args[2], mustWork = TRUE)
+    shift(2)
+  } else if (args[1] == "-m") {
+    stopifnot(length(args) >= 2)
+    mirror <- args[2]
+    shift(2)
+  } else if (args[1] == "-f") {
+    force <- TRUE
+    shift(1)
+  } else if (args[1] == "--help") {
+    print_help()
+    q(save = "no", status=1)
+  } else {
+    packages <- append(packages, args[1])
+    shift(1)
+  }
+}
+
+if (length(packages) == 1 && file.exists(packages)) {
+  packages <- trimws(readLines(packages), "both")
+  packages <- packages[nchar(packages) > 0]
+}
+
+install_cran_packages(from=packages, libdir=libdir, destdir=destdir, mirror=mirror, force=force)
